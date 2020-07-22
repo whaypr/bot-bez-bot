8657a770c26c0df3fc0b95bfc0601626e687b4bd
Patrik Drbal <drbalpat@fit.cvut.cz>
2020-07-22 21:42:16 +0200

    update discord.py

--- bot-bez-bot/__main__.py
+++ bot-bez-bot/__main__.py
@@ -2,4 +2,4 @@ from bot import client
 
 import commands, events, message_deleting
 
-client.run('NzI5NjU0MzE0NzI4OTQ3Nzgy.XwMGAA.frhAFS1Ven0nmz7Qn8nq6yrYrFE')
+client.run('NzI5NjU0MzE0NzI4OTQ3Nzgy.XxiU2Q.mt2RWjN1nZUNC6G6L3yQKhI39Ms')
